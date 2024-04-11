811b41b54ed6e00022e85db6a4b95dc105424349
Bia <82560900+BiatrizR@users.noreply.github.com>
2024-04-11 17:50:05 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var astronaut, astrounautIdie
+
+var ground, groundImg
+
+function setup() {
+  canvas = createCanvas(1600, 800);
+  //TODO 1: centralizar canvas
+  canvas.position(410, 200, 'fixed');
+ // canvas.center();
+
+
+}
+
+
+
+function draw() {
+  background("lightblue"); 
+  
+
+  
+
+  drawSprites();
+}
